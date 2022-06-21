10c4abc879eadcbc8a6a64b2dd2a3220fab2fe08
Tran Huu Nhat Huy <huy9515@gmail.com>
2022-06-21 16:03:23 +0900

    further improvements in synthetic generator

--- data/synthetic_spectrum_generator.py
+++ data/synthetic_spectrum_generator.py
@@ -43,38 +43,51 @@ def synthSpectrumGenerate():
     isotope = "1"
     pressure = 0.005
     Tgas = 3000
+    bound_Tgas = [2000, 4000]
     Tvib = ""
+    bound_Tvib = []
     Trot = ""
+    bound_Trot = []
     mole_fraction = 0.01
     path_length = 1
-    slit = 5
+    slit = 1.4
     slit_unit = "nm"
     name = f"synth-{molecule}-{isotope}-{from_wl}-{to_wl}-{wunit}-{pressure}-t{Tgas}-v{Tvib}-r{Trot}-p{path_length}-sl{slit}{slit_unit}"
 
     # -------------------------------------- EDIT HERE! -------------------------------------- #
 
     # Generate synthetic spectrum based on parameters for ground-truth
-    s = calc_spectrum(
-        from_wl,
-        to_wl,
-        wunit = wunit,
-        Tgas = Tgas,
-        molecule = molecule,
-        isotope = isotope,
-        pressure = pressure,  # bar
-        mole_fraction = mole_fraction,
-        path_length = path_length,  # cm
-        databank = "hitran",
-        name = name        
-    ).apply_slit(slit, slit_unit)
+    s = (
+        calc_spectrum(
+            from_wl,
+            to_wl,
+            wunit = wunit,                  # "cm-1" or "nm"
+            Tgas = Tgas,                    # in K
+            molecule = molecule,
+            isotope = isotope,
+            pressure = pressure,            # in bar
+            mole_fraction = mole_fraction,
+            path_length = path_length,      # in cm
+            wstep = "auto",
+            databank = "hitran",
+            name = name)
+        .apply_slit(slit, slit_unit)        # Apply slit function to reduce resolution
+        .offset(-0.2, "nm")                 # Make the spectrum to feature offset, typically 0.2 nm
+    )
     # Show it to make sure nothing goes wrong
     s.plot(show = True)
 
 
-    # Add some noises into the spectrum. The noise should be around 1% of max radiance
     s_wav, s_val = s.get('radiance')
-    noise_scale = max([val for val in s_val if not(math.isnan(val))]) * 0.01 # Prevent NaN
+
+    # Reproduce noises for the spectrum. The noise should be around 0.5% of max radiance
+    noise_scale = max([val for val in s_val if not(math.isnan(val))]) * 0.005 # Prevent NaN
     s_val += np.random.normal(size = s_val.size, scale = noise_scale)
+
+    # Reproduce dilatation by applying non-linear transformation, with scale 0.66% of deviation
+    wav_mean = np.mean([wav for wav in s_wav if not(math.isnan(wav))])
+    print(wav_mean)
+    s_wav = wav_mean + (s_wav - wav_mean) * 1.0066
 
 
     # Re-generate noise-added spectrum
@@ -87,7 +100,7 @@ def synthSpectrumGenerate():
     spectrum_dir = f"./{spec_type}/spectrum/"
 
     if wunit == "cm-1":
-        wunit = "cm" # This is to avoid mistakes with the pressure value next to it
+        wunit = "cm" # To avoid mistakes with the pressure value next to it when reading fileName
     fileName_spec = f"{name}.spec"
 
     s_synth.store(spectrum_dir + fileName_spec)
@@ -98,26 +111,31 @@ def synthSpectrumGenerate():
     gt_dir = f"./{spec_type}/ground-truth/{fileName_json}" # Grouth-truth file directory
 
     written_info = {
-    "fileName": fileName_spec,
-    "molecule": molecule,
-    "isotope": isotope,
-    "from_wavelength": from_wl,
-    "to_wavelength": to_wl,
-    "pressure": pressure,
-    "mole_fraction": mole_fraction,
-    "path_length": path_length,
-    "slit": f"{slit} {slit_unit}",
-    "fit": {
-        "Tgas": Tgas,
-        "Tvib": Tvib,
-        "Trot": Trot,
-    }
+        "fileName": fileName_spec,
+        "molecule": molecule,
+        "isotope": isotope,
+        "from_wavelength": from_wl,
+        "to_wavelength": to_wl,
+        "pressure": pressure,
+        "mole_fraction": mole_fraction,
+        "path_length": path_length,
+        "slit": f"{slit} {slit_unit}",
+        "fit": {
+            "Tgas": Tgas,
+            "bound_Tgas": bound_Tgas,
+            "Tvib": Tvib,
+            "bound_Tvib": bound_Tvib,
+            "Trot": Trot,
+            "bound_Trot": bound_Trot
+        }
     }
     # Remove temperatures that does not include in the fitting process (such as when LTE)
     if written_info["fit"]["Tvib"] == "":
         written_info["fit"].pop("Tvib")
+        written_info["fit"].pop("bound_Tvib")
     if written_info["fit"]["Trot"] == "":
         written_info["fit"].pop("Trot")
+        written_info["fit"].pop("bound_Trot")
 
     # Write the information of spectrum into a new JSON file
     with open(gt_dir, 'w') as f:

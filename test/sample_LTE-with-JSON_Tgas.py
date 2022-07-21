3ac5937ca36b0116bcf6714cdeae8347d14b99bc
Tran Huu Nhat Huy <huy9515@gmail.com>
2022-07-21 14:16:44 +0900

    add test files and sample notebooks

--- test/sample_LTE-with-JSON_Tgas.py
+++ test/sample_LTE-with-JSON_Tgas.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from modules.fitting_module import fit_spectrum
-import matplotlib.pyplot as plt
+from radis import load_spec
 
+
+# Get JSON file path (note that the experimental spectrum file MUST BE IN THE SAME FOLDER containing JSON file)
 JSON_path = "./test_dir/test_JSON_file.json"
-s_best, result, log = fit_spectrum(JSON_path)
+
+
+# Conduct the fitting process!
+s_best, result, log = fit_spectrum(input_file = JSON_path)
+
 
 # Now investigate the log
 
